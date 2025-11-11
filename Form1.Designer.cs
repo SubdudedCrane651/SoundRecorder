798cbb9ead792000e3ae712b41022e811bf2fda8
SubduedCrane651 <rchrdperreault@gmail.com>
2025-11-11 16:59:57 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace SoundRecorder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRecord = new System.Windows.Forms.Button();
+            this.btnStopandSave = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnRecord
+            // 
+            this.btnRecord.Location = new System.Drawing.Point(177, 183);
+            this.btnRecord.Name = "btnRecord";
+            this.btnRecord.Size = new System.Drawing.Size(75, 23);
+            this.btnRecord.TabIndex = 0;
+            this.btnRecord.Text = "Record";
+            this.btnRecord.UseVisualStyleBackColor = true;
+            this.btnRecord.Click += new System.EventHandler(this.btnRecord_Click);
+            // 
+            // btnStopandSave
+            // 
+            this.btnStopandSave.Location = new System.Drawing.Point(313, 182);
+            this.btnStopandSave.Name = "btnStopandSave";
+            this.btnStopandSave.Size = new System.Drawing.Size(75, 23);
+            this.btnStopandSave.TabIndex = 1;
+            this.btnStopandSave.Text = "Stop & Save";
+            this.btnStopandSave.UseVisualStyleBackColor = true;
+            this.btnStopandSave.Click += new System.EventHandler(this.btnStopandSave_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(451, 183);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(177, 240);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Recording....";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.btnStopandSave);
+            this.Controls.Add(this.btnRecord);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRecord;
+        private System.Windows.Forms.Button btnStopandSave;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using CSCore;
+using CSCore.SoundIn;
+using CSCore.Codecs.WAV;
+
+namespace SoundRecorder
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("winmm.dll")]
+        private static extern int mciSendString(string MciComando, string MciRetorno, int MciRetornoLeng, int CallBack);
+
+        string record = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //sound card record
+
+        WasapiCapture capture;
+        WaveWriter w;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+
+        }
+
+        private void btnRecord_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            capture = new WasapiLoopbackCapture();
+            //initialize the selected device for recording
+            capture.Initialize();
+            //mciSendString("open new type waveaudio alias recsound", null, 0, 0);
+            //mciSendString("record recsound", null, 0, 0);
+            string datestr = string.Format("{0:yyyymmddHHmm }", DateTime.Now);
+            w = new WaveWriter("soundcard" + datestr + ".wav", capture.WaveFormat);
+
+            capture.DataAvailable += (s, e1) =>
+            {
+                //save the recorded audio
+                w.Write(e1.Data, e1.Offset, e1.ByteCount);
+            };
+
+            //start recording
+            capture.Start();
+
+        }
+
+        private void btnStopandSave_Click(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            capture.Stop();
+
+            w.Dispose();
+            capture.Dispose();
+            
+            //mciSendString("pause recsound", null, 0, 0);
+            //SaveFileDialog save = new SaveFileDialog();
+            //save.Filter = "wave|*.wav";
+
+            //if (save.ShowDialog() == DialogResult.OK)
+            //{
+
+            //    mciSendString("save recsound " + save.FileName, null, 0, 0);
+            //    mciSendString("close recsound", null, 0, 0);
+            //}
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (record == "")
+            {
+                OpenFileDialog open = new OpenFileDialog();
+                open.Filter = "Wave|*.wav";
+                if (open.ShowDialog() == DialogResult.OK) { record = open.FileName; }
+            }
+            mciSendString("play " + record, null, 0, 0);
+        }
+    }
+}
